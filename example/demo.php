205d23b0a1868cd1a2447edbd280f9f2f09c52b0
yaojun.deng <yaojun.deng@yeepay.com>
2016-07-14 15:14:08 +0800

    修改demo

--- example/demo.php
+++ example/demo.php
@@ -1,7 +1,7 @@
 <?php
 function findChannelInfoByAppId(){
 	include_once('MobpexSdk.php');   
-	$client = new MobpexClient('https://220.181.25.235/yop-center',//Mobpex对外提供的地址 , 'http://172.17.102.188:8093/yop-center'
+	$client = new MobpexClient('https://www.mobpex.com/yop-center',//Mobpex对外提供的地址 , 'http://172.17.102.188:8093/yop-center'
 						'/rest/v1.0/query/findChannelInfoByAppId',  //接口对应的uri
 						false,                                      //是否需要进行证书验证
 						'15122404366710489367',                     //appid15122404163671048936
@@ -17,7 +17,7 @@ function findChannelInfoByAppId(){
 
 function unifiedOrder(){
 	include_once('MobpexSdk.php'); 
-	$client = new MobpexClient('https://220.181.25.235/yop-center',// Mobpex对外提供的地址  'https://220.181.25.235/yop-center',       
+	$client = new MobpexClient('https://www.mobpex.com/yop-center',// Mobpex对外提供的地址  'https://www.mobpex.com/yop-center',       
 						'/rest/v1.0/pay/unifiedOrder',  //接口对应的uri
 						false,                                      //是否需要进行证书验证
 						'15122404366710489367',                     //appid
@@ -31,11 +31,7 @@ function unifiedOrder(){
 					   'payChannel' => "WECHAT",
 					   'payCurrency' => "CNY",
 					   'amount' => "200.1",
-					   'deviceId' => "1183224081",
-					   'deviceType' => "MOBILE",
-					   'tradeNo' => "1134376521",
-					   'requestIp' => "127.0.0.1",
-					   'requestIdentification' => "iPhone 7s");
+					   'tradeNo' => "1134376521");
 	$client->set_param('prePayRequest',json_encode($prePayRequest, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
 	$result = $client->execute(); // 对于不需要session的api，则可以不用session参数  
 	if($client->validSign($result)){ //返回结果的验签
@@ -45,7 +41,7 @@ function unifiedOrder(){
 
 function refund(){
 	include_once('MobpexSdk.php'); 
-	$client = new MobpexClient('https://220.181.25.235/yop-center',//Mobpex对外提供的地址  'https://220.181.25.235/yop-center', 
+	$client = new MobpexClient('https://www.mobpex.com/yop-center',//Mobpex对外提供的地址  'https://www.mobpex.com/yop-center', 
 						'/rest/v1.0/pay/refund',  //接口对应的uri
 						false,                                      //是否需要进行证书验证
 						'15122404366710489367',                     //appid
@@ -56,12 +52,8 @@ function refund(){
 	$prePayRequest = array("tradeNo" => "656113769613",//商户系统的支付请求流水号
     					   "amount" => "3.00",//合计金额，精确到小数点后2位
     					   "description"=> "我要退款~~~",//描述
-    					   "deviceId"=> "1183224081",//终端唯一编号
-    					   "deviceType"=> "MOBILE",//终端类型
-    					   "requestCustomerId"=> "",
-    					   "refundNo"=> "20485228279",//商户系统的退款请求流水号
-    					   "requestIp"=> "127.0.0.1",//请求IP
-    					   "requestIdentification"=> "iPhone 7s");
+    					   "refundNo"=> "20485228279"//商户系统的退款请求流水号
+    					 );
 	$client->set_param('refundRequest',json_encode($prePayRequest, JSON_UNESCAPED_UNICODE| JSON_UNESCAPED_SLASHES));  
 	$result = $client->execute(); // 对于不需要session的api，则可以不用session参数  
 	
@@ -72,7 +64,7 @@ function refund(){
 
 function queryPaymentOrder(){
 	include_once('MobpexSdk.php'); 
-	$client = new MobpexClient('https://220.181.25.235/yop-center',//Mobpex对外提供的地址  'https://220.181.25.235/yop-center',    
+	$client = new MobpexClient('https://www.mobpex.com/yop-center',//Mobpex对外提供的地址  'https://www.mobpex.com/yop-center',    
 						'/rest/v1.0/pay/queryPaymentOrder',  //接口对应的uri
 						false,                                      //是否需要进行证书验证
 						'15122404366710489367',                     //appid
@@ -89,7 +81,7 @@ function queryPaymentOrder(){
 
 function queryRefundOrder(){
 	include_once('MobpexSdk.php'); 
-	$client = new MobpexClient('https://220.181.25.235/yop-center',  //Mobpex对外提供的地址  'https://220.181.25.235/yop-center',   
+	$client = new MobpexClient('https://www.mobpex.com/yop-center',  //Mobpex对外提供的地址  'https://www.mobpex.com/yop-center',   
 						'/rest/v1.0/pay/queryRefundOrder',  //接口对应的uri
 						false,                                      //是否需要进行证书验证
 						'15122404366710489367',                     //appid
